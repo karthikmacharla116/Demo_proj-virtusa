45c087b7492a78a681c48f7d4e0373ff4ce824fe
karthikmacharla116 <97028010+karthikmacharla116@users.noreply.github.com>
2022-03-08 18:13:26 +0530

    Delete Hello001.java

--- merging_project/src/com/hello/Hello001.java
+++
@@ -1,9 +0,0 @@
-package com.hello;
-
-public class Hello001 {
-	public static void main(String[] args) {
-		System.out.println("This is my java project!");
-		System.out.println("Yes I got this file in my remote repository..");
-		System.out.println("wow its really good!...");
-	}
-}
