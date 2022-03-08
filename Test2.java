0319986f7eaba8b5d45ac268e27fc07970c52432
karthikmacharla116 <97028010+karthikmacharla116@users.noreply.github.com>
2022-03-08 19:11:02 +0530

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,11 @@
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Everyone!");
+		System.out.println("Hai");
+		System.out.println("Wow nice");
+
+	}
+
+}
